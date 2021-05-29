048aebffe3b40c6c4c86c703e6d660f617c4f48c
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2021-05-29 19:52:33 +0300

    Cards added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import TabGroup from "./components/TabGroup/TabGroup";
 import HomePage from "./components/__pages/HomePage/HomePage";
+import Cards from "./components/Card/Cards";
+import React from "react";
 
 function App() {
   // WP_React
+    //<HomePage/>
 
   return (
     <div className="App">
-      <TabGroup/>
-      <HomePage/>
+        <TabGroup/>
+        <Cards/>
     </div>
   );
 }
---
+++ src/components/Card/Cards.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import s from './Card.module.css'
+
+const Cards = () => {
+    const [cardList, setCardList] = useState([
+        {id: 1, order: 3, text: 'Карточка 3'},
+        {id: 2, order: 1, text: 'Карточка 1'},
+        {id: 3, order: 2, text: 'Карточка 2'},
+        {id: 4, order: 4, text: 'Карточка 4'},
+    ])
+    return (
+        <div className='card'>
+            {cardList.map(card =>
+                <div className={s.card}>
+                    {card.text}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Cards
