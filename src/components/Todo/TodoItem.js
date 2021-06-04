2096fb8100166ba8b5fd97a91db3ddbe90763e26
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2021-06-04 16:22:07 +0300

    TodoItem added

---
+++ src/components/Todo/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
+export default function TodoItem() {
+    return <li>todo</li>
+}
