c9086843725b18a69ec46de99bd81a4dbe6dafd2
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2021-06-04 17:10:12 +0300

    rm style

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+}
+
 function TodoItem({todo, index}) {
     return (
-        <li>
+        <li style={styles.li}>
             <span>
-                <input type="checkbox"/>
+                <input type="checkbox" style={styles.input}/>
+                <strong>{index + 1}</strong>
+                {todo.title}
             </span>
-            <strong>
-                {index + 1}
-            </strong>
-            {todo.title}
+            <button className='rm'>&times;</button>
         </li>)
 }
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired,
     index: PropTypes.number
-
 }
 
 export default TodoItem
